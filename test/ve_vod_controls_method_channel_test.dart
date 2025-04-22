bdca33aae4272d7687493a0dfdde0307d1abfb3f
LiWeNHuI <sdgrlwh@163.com>
2025-04-22 12:01:00 +0800

    chore;

--- test/ve_vod_controls_method_channel_test.dart
+++ test/ve_vod_controls_method_channel_test.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/services.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:ve_vod_controls/src/interface/ve_vod_controls.dart';
+import 'package:ve_vod_controls/interface/ve_vod_controls.dart';
 
 void main() {
   TestWidgetsFlutterBinding.ensureInitialized();
